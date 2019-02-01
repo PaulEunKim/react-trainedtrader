ac676d4c723d9dc4ed415bb9c8f5564c88a1d381
Paul Kim <paul.eun.kim@gmail.com>
2019-02-01 11:40:47 -0700

    edited table

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import './App.css';
 import Body from './components/body.js';
 import Header from './components/header.js';
 import Footer from './components/footer.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <>
         <Header></Header>
-        <Body></Body>
-        <Footer></Footer>
-      </div>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/about">이런 문구들이 익숙하신가요?</Link>
+              </li>
+              <li>
+                <Link to="/topics">첫번재 - STOP LOSS 세팅 방법</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={Body} />
+            <Route path="/about" component={Header} />
+            <Route path="/topics" component={Footer} />
+          </div>
+        </Router>
+        <Footer>
+        </Footer>
+      </>
     );
   }
 }
